f9a28785a782fadf14e95ca0a9eb1a0ec6c099a4
Mohamedhashir Hussein <hashirhillowle@gmail.com>
2022-09-21 18:30:16 -0700

    Numbers have life; They are not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
